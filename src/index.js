450096a1a5dbe148d81c89a49fbd691b0de4c911
lmwang27 <52217891+lmwang27@users.noreply.github.com>
2021-01-28 14:32:01 -0800

    adding rendering

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+var count = 0;
+function increase() {
+  console.log("I got clicked.");
+  count += 1;
+  ReactDOM.render(
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>,
+    document.getElementById("root")
+  );
+}
+
 ReactDOM.render(
   <div className="container">
-    <h1>0</h1>
-    <button>+</button>
+    <h1>{count}</h1>
+    <button onClick={increase}>+</button>
   </div>,
   document.getElementById("root")
 );
